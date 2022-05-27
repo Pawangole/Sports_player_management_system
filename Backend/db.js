8bb7936f5c7789760c766f48edcef1ee24e6b5f8
Pawangole <68113963+Pawangole@users.noreply.github.com>
2022-05-27 16:43:36 +0530

    commit

---
+++ Backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/meanDB', (err)=>{
+    if(!err){
+        console.log('connection successful')
+    }else{
+        console.log('error in connection' + err)
+    }
+})
+ 
+module.exports = mongoose;
+
