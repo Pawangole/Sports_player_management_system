cc76b9fe209abdd15240244b72926c1b2091f61f
Pawangole <68113963+Pawangole@users.noreply.github.com>
2022-05-27 17:22:57 +0530

    index.js

---
+++ Backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const mongoose = require('./db.js');
+
+const app = express();
+app.use(bodyparser.json());
+app.use(cors({origin:'http://localhost:4200'}));
+
+app.listen(3000, ()=>console.log('server started at port: 3000'));
