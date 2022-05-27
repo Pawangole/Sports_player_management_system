ffe2cde65efccde9aeacad9eca1ab663c9d69937
Pawangole <68113963+Pawangole@users.noreply.github.com>
2022-05-27 16:45:50 +0530

    add

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/meanDB', (err)=>{
     if(!err){
-        console.log('connection successful')
+        console.log('db connection successful')
     }else{
         console.log('error in connection' + err)
     }
