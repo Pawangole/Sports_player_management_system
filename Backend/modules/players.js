c625083d9b1aab11526b8a5554bd9cec1ee014c1
Swathireddy123456 <70442679+Swathireddy123456@users.noreply.github.com>
2022-05-28 15:03:26 +0530

    Update players.js

--- Backend/modules/players.js
+++ Backend/modules/players.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 const player = mongoose.model('players', {
-    name :
+    playerId:{type:Number},
+     playerName:{type:String},
+     userName:{type:String},
+     password:{type:String},
+     phoneNo:{type:Number},
+     playerEquipments:{type:String},
+     registrationDate:{type:Date},
+     sportsPlay:{type:String}
     
-})
+});
+module.exports={Player};
