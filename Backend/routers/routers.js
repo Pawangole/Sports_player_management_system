bc9cdb502c96e87cb1e187a4a4842d19ba244878
Swathireddy123456 <70442679+Swathireddy123456@users.noreply.github.com>
2022-05-28 16:03:03 +0530

    Update routers.js

--- Backend/routers/routers.js
+++ Backend/routers/routers.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const {Player}=require('../models/players');
+const {Player}=require('../models/Player');
 
 router.get('/',(req,res)=>{
     Player.find((err,docs)=>{
