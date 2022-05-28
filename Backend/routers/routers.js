ac7c787c6d4a8a9c64bfcf0d748ee123fa7dcd8f
Swathireddy123456 <70442679+Swathireddy123456@users.noreply.github.com>
2022-05-28 19:07:23 +0530

    Update routers.js

--- Backend/routers/routers.js
+++ Backend/routers/routers.js
@@ -35,4 +35,48 @@ router.post('/',(req,res)=>{
         }
     });
 });
+router.put('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.param.id))
+    return res.status(400).send('No record with given id:${req.params.id}');
+
+var play={
+    playerId:req.body.playerId,
+ playerName:req.body.playerName,
+ userName:req.body.userName,
+ password:req.body.password,
+ phoneNo:req.body.phoneNo,
+ playerEquipments:req.body.playerEquipments,
+ registrationDate:req.body.registrationDate,
+ sportsPlay:req.body.sportsPlay
+};
+Player.findByIdAndUpdate(req.params.id,{$set:play},{new:true},(err,doc)=>{
+    if(!err)
+    {
+        res.send(doc)
+       
+    }
+    else
+    {
+        console.log('Error i Post Data'+err)
+    }
+});
+});
+
+router.delete('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.param.id))
+    return res.status(400).send('No record with given id:${req.params.id}');
+
+    Player.findByIdAndRemove(req.params.id,{$set:play},{new:true},(err,doc)=>{
+        if(!err)
+        {
+            res.send(doc)
+           
+        }
+        else
+        {
+            console.log('Error i Post Data'+err)
+        }
+    });
+
+});
 module.exports=router;
