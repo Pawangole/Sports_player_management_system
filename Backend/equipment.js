c8e97aaf8d169c1c0c7bab32f7b6171578167051
Preethy Meena <38650903+PreethySivakumar@users.noreply.github.com>
2022-05-30 11:40:45 +0530

    Delete equipment.js

--- Backend/equipment.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-
-var equipment = mongoose.model('equipment',{
-    equipment_id:{type:Number},
-    equipment_name:{type:String},
-    equipment_fee:{type:Number}
-})
-
-module.exports = {equipment}
