2ed5531f84a8625df53f1dfed1e42014e0e776a4
Pawangole <68113963+Pawangole@users.noreply.github.com>
2022-05-30 11:25:42 +0530

    sports

---
+++ Backend/modules/Sports.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+var sport = mongoose.model('sport',{
+    sport_id:{type:Number},
+    sport_name:{type:String},
+    sport_fee:{type:Number} 
+})
+
+module.exports = {sport}
