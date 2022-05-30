63a7738c3796cf55e11d0fc67934968b8cbec2d8
Swathireddy123456 <70442679+Swathireddy123456@users.noreply.github.com>
2022-05-30 16:50:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,17 @@ const bodyparser = require('body-parser');
 const cors = require('cors');
 
 const mongoose = require('./db.js');
-const routers=require('./routers/routers.js');
+const player=require('./routers/player.js');
+const sports=require('./routers/sports.js');
+const equipment=require('./routers/equipment.js');
+const  admin  = require('./routers/admin.js');
 
 const app = express();
 app.use(bodyparser.json());
 app.use(cors({origin:'http://localhost:4200'}));
 
 app.listen(3000, ()=>console.log('server started at port: 3000'));
-app.use('/player',routers);
+app.use('/player',player);
+app.use('/sports',sports);
+app.use('/equipments',equipment);
+app.use('/admin',admin);
