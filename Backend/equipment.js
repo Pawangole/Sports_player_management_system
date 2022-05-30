13ada025ee9b8b7086558cd250585ee62eca0365
Swathireddy123456 <70442679+Swathireddy123456@users.noreply.github.com>
2022-05-30 10:22:35 +0530

    Update equipment.js

--- Backend/equipment.js
+++ Backend/equipment.js
@@ -3,6 +3,7 @@ let mongoose = require('mongoose');
 var equipment = mongoose.model('equipment',{
     equipment_id:{type:Number},
     equipment_name:{type:String},
+    equipment_fee:{type:Number}
 })
 
 module.exports = {equipment}
